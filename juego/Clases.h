bab0b256dc3445f5187c36f0f1cf88cb8fed36ed
Raikert <ezequielrickert@gmail.com>
2019-10-30 19:52:46 -0300

    AL FINAL TODO SE PUEDE LOGRAR GENTE VAMOOO!!!! xddd

--- juego/Clases.h
+++ juego/Clases.h
@@ -41,7 +41,7 @@ public:
             texto.setFillColor(color);
         }
     }
-    void setFormato_texto (const string& letra)
+    void setFormato_de_letra (const string& letra)
     {
         if (!formato_de_letra.loadFromFile(letra))
             formato_cargado=false;
@@ -67,6 +67,10 @@ public:
     void setColor(const Color& color)
     {
         texto.setFillColor(color);
+    }
+    void setPosicion (float x,float y)
+    {
+        texto.setPosition(x,y);
     }
 };
 
