4eabcc7705a3d3c527ea3aa0bccff01e7e6403a3
Raikert <ezequielrickert@gmail.com>
2019-12-08 02:34:27 -0300

    Solo comentarios

--- juego/main.cpp
+++ juego/main.cpp
@@ -6,9 +6,9 @@
 #include "Collision.hpp"
 #include "funciones.h"
 #include "Clases.h"
-using namespace sf; ///para no poner sf::
+using namespace sf; //para no poner sf::
 using namespace std;
-using namespace Collision; ///para no poner Collision::
+using namespace Collision; //para no poner Collision::
 #include "headers/Movimientos.h"
 #include "headers/Colision_Danio.h"
 #include "headers/Sprites.h"
@@ -20,3 +20,14 @@ int main()
     //sino nunca sabriamos si un archivo se cargo correctamente, matamos todos los pajaros con un solo return jeje.
     return juego();
 }
+
+/// No tengo palabras, solo veo lo que hicimos hasta ahora ,
+///estoy muy orgulloso de todos nosotros, la idea, el empeño
+///que le ponemos cada dia a todo, y solo pienso en aquella idea
+///de fede que comenzo todo este proyecto, que nos impulso a ser mas
+///que unos simples programadores, jamas voy a olvidar esto,
+///no esta a la altura de muchos tower defense pero, con tan solo ver como
+///funciona todo, me hace sentir que el esfuerzo que todos hacemos a la larga
+///vale la pena, y a pesar de que dejemos cosas para mas tarde, como es mi caso con Arso,
+///no siento remordimientos cuando aprendo algo nuevo cada dia sobre este mundo tan maravilloso
+///que es programar.
